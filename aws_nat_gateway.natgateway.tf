5763062ec5cf368e65667a2fe6a3ba45b6f21d53
James Woolfenden <james.woolfenden@gmail.com>
2019-01-05 15:26:17 +0000

    fix the readme

--- aws_nat_gateway.natgateway.tf
+++ aws_nat_gateway.natgateway.tf
@@ -2,9 +2,6 @@ resource "aws_nat_gateway" "natgateway" {
   count         = 3
   allocation_id = "${element(aws_eip.nateip.*.id, count.index)}"
   subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
-
-  #public_ip     = "${element(aws_eip.nateip.*.public_ip, count.index)}"
-
-  depends_on = ["aws_internet_gateway.gw"]
-  tags       = "${merge(var.common_tags, map("Name", "${upper(var.account_name)}-AZ${count.index+1}"))}"
+  depends_on    = ["aws_internet_gateway.gw"]
+  tags          = "${merge(var.common_tags, map("Name", "${upper(var.account_name)}-AZ${count.index+1}"))}"
 }
