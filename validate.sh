caa05ea49de96421f73c07fbe31d81c6fe291cb6
James Woolfenden <James.Woolfenden@slalom.com>
2019-07-13 21:23:54 +0100

    merge

--- validate.sh
+++ validate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cd example/exampleA || return
+<<<<<<< Updated upstream
 rm .terraform/ -fr
+=======
+rm .terraform/ -fr 
+>>>>>>> Stashed changes
 terraform init
 terraform validate
