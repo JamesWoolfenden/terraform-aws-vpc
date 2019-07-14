a3e50812e822baf8fd72eb1d48e2d706e9e56065
James Woolfenden <james.woolfenden@gmail.com>
2019-07-14 16:00:41 +0100

    Draft of v0.12

--- validate.sh
+++ validate.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 cd example/exampleA || return
-<<<<<<< Updated upstream
-rm .terraform/ -fr
-=======
 rm .terraform/ -fr 
->>>>>>> Stashed changes
 terraform init
 terraform validate
