a00e87fd32fb255ca3e83c10be83db182cd95150
pangchong <pang.chong@wealth-park.com>
2020-01-11 16:22:46 +0000

    Changed quoted references.

--- aws_nat_gateway.natgateway.tf
+++ aws_nat_gateway.natgateway.tf
@@ -1,7 +1,7 @@
 resource "aws_nat_gateway" "natgateway" {
   count         = var.subnets
   allocation_id = element(aws_eip.nateip.*.id, count.index)
-  depends_on    = ["aws_internet_gateway.gw"]
+  depends_on    = aws_internet_gateway.gw
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   tags = merge(var.common_tags,
   map("Name", "${upper(var.account_name)}-AZ${count.index + 1}"))
