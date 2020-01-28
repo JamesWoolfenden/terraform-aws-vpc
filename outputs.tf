e429984b2fbff4625dc61ca110f5bbfd043c95ef
James Woolfenden <james.woolfenden@gmail.com>
2020-01-28 07:28:57 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,12 +13,27 @@ output "vpc_id" {
   description = "The ID of your selected VPC"
 }
 
+output "vpc" {
+  value       = aws_vpc.main
+  description = "The Full VPC"
+}
+
 output "private_subnets" {
   value       = aws_subnet.private.*.id
   description = "The IDs of your private subnets"
 }
 
+output "private" {
+  value       = aws_subnet.private
+  description = "The Private subnets"
+}
+
 output "public_subnets" {
   value       = aws_subnet.public.*.id
   description = "The IDs of your public subnets"
+}
+
+output "public" {
+  value       = aws_subnet.public
+  description = "The Public Subnets"
 }
