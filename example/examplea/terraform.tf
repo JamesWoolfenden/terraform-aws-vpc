814a3e895dea59fa729a1f7d2d6fde7189ab8a20
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-29 10:31:53 +0100

    cleanup

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,10 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      version = "3.39.0"
+      version = "3.42.0"
       source  = "hashicorp/aws"
     }
     template = {
+      source  = "hashicorp/template"
       version = "2.2.0"
     }
   }
